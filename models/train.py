bac5c8d10fcbb5ad276a749f17ee68d0a615bbcd
lllllllll <63034312+lllllllll-3154@users.noreply.github.com>
2022-07-11 16:44:00 +0800

    third commit
    now the best model will be saved seperately

--- models/train.py
+++ models/train.py
@@ -16,6 +16,7 @@ splits_path = 'preprocessed_data'
 saved_path = os.path.join(root_dir,"saved_model")
 log_path = os.path.join(root_dir,"logs")
 log_file = os.path.join(log_path,"training_logs.log")
+pre_trained_path = os.path.join(root_dir,"pre_trained_model")
 
 def get_logger(filename, verbosity=1, name=None):
     level_dict = {0: logging.DEBUG, 1: logging.INFO, 2: logging.WARNING}
@@ -35,16 +36,22 @@ def get_logger(filename, verbosity=1, name=None):
 
     return logger
 
-def main(epochs,lr,samples,batch_size,selected_num):
+def main(epochs,lr,samples,batch_size,selected_num,pre_trained=None):
     logger = get_logger(log_file)
     mytraindata = MyDataset(root_dir,splits_path, selected_num, "train",samples=samples)
     myvalidatedata = MyDataset(root_dir,splits_path,selected_num, "validate",samples=samples)
-    mynn = AudioAlexNet().to(cuda)
+    if pre_trained:
+        print("loading pre_trained model: ",pre_trained)
+        pre_trained_model_path = os.path.join(pre_trained_path,pre_trained)
+        mynn=torch.load(pre_trained_model_path)
+    else:
+        mynn = AudioAlexNet().to(cuda)
     loss = CrossEntropyLoss().to(cuda)
     optim = Adam(mynn.parameters(),lr=lr)
     train_loader = DataLoader(mytraindata, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
     validate_loader = DataLoader(myvalidatedata, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
     total_len = len(myvalidatedata)
+    best_acc = 0.5
     for epoch in range(epochs):
         print("current epoch: ",epoch)
         running_loss= 0
@@ -70,7 +77,13 @@ def main(epochs,lr,samples,batch_size,selected_num):
                 validation_loss += result
                 correct_num = sum(targets.eq(output.argmax(dim=1)))
                 total_correct+=correct_num
-        accuracy =  total_correct/total_len     
+        accuracy =  total_correct/total_len   
+        if accuracy> best_acc:
+            best_acc = accuracy
+            best_name = "best_acc.pth"
+            best_name_path = os.path.join(saved_path,best_name)
+            torch.save(mynn,best_name_path)
+            print("best accuracy model saved in epoch: ",epoch)            
         logger.info('Epoch:[{}/{}]\t validation_loss={:.5f}\t training_loss = {:.5f}\t acc={:.5f}'.format(epoch , epochs, validation_loss.item(),running_loss.item(), accuracy.item() ))
         if (epoch%10) == 0:
             filename = "model_epoch"+str(epoch)+".pth"
@@ -90,6 +103,7 @@ if __name__ == "__main__":
     parser.add_argument("--samples",type=int,default=2048,help="samples being used from training data")
     parser.add_argument("--batch_size",type=int,default=256,help="batch size")
     parser.add_argument("--selected_num",type=int,default=0,help="which of the testing sample to use, from 0-4 ")
+    parser.add_argument("--pre_trained",type=str,default=None,help="pre_trained_model")
 
     args = parser.parse_args()
     
@@ -98,9 +112,10 @@ if __name__ == "__main__":
     samples = args.samples
     batch_size = args.batch_size
     selected_num = args.selected_num
+    pre_trained = args.pre_trained
     print("start training...")
     
-    main(epochs,lr,samples,batch_size,selected_num)
+    main(epochs,lr,samples,batch_size,selected_num,pre_trained)
     
     print("training is over...")
     
